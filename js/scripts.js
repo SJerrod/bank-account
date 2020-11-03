6f5e3be458473930d9b4434e532ee5e0ff552fe3
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-03 15:41:23 -0800

    keep a running total and allow multiple transactions
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,61 @@
 // Business Logic
-function Account(name, depositInitial, deposit, withdraw, total) {
+function Account(name, depositInitial, total) {
   this.name = name;
   this.depositInitial = depositInitial;
+  this.total = 0;
+}
+
+function Change(deposit, withdraw) {
   this.deposit = deposit;
   this.withdraw = withdraw;
-  this.total = 0;
 }
 
 Account.prototype.initialPush = function(newAccount) {
   this.total = this.depositInitial;
 }
 
-Account.prototype.depositAmount = function(newAccount) {
-  if (isNaN(this.deposit)) {
-    this.deposit = 0;
+Account.prototype.depositAmount = function(change) {
+  if (isNaN(change.deposit)) {
+    change.deposit = 0;
   } else {
-    this.total += this.deposit;
+    this.total += change.deposit;
   }
   return this.total;
 }
 
-Account.prototype.withdrawAmount = function(newAccount) {
-  if (isNaN(this.withdraw)) {
-    this.withdraw = 0;
+Account.prototype.withdrawAmount = function(change) {
+  if (isNaN(change.withdraw)) {
+    change.withdraw = 0;
   } else {
-  this.total -= this.withdraw;
+  this.total -= change.withdraw;
   }
   return this.total;
 }
 
 // User Logic
 $(document).ready(function() {
-  $(".change").submit(function(event) {
+  $(".initial").submit(function(event) {
     event.preventDefault();
 
     let name = $("input#name").val();
     let depositInitial = parseInt($("input#initial").val());
-    let deposit = parseInt($("input#deposit").val());
-    let withdraw = parseInt($("input#withdraw").val());
-    let newAccount = new Account(name, depositInitial, deposit, withdraw);
+    let newAccount = new Account(name, depositInitial);
     
     newAccount.initialPush();
-    newAccount.depositAmount();
-    newAccount.withdrawAmount();
-    console.log(newAccount);
-
-    $(".balance").text(newAccount.total);
-    
+    $(".balance").text(newAccount.total)
+    $(".change").submit(function(event) {
+      event.preventDefault();
+      
+      let deposit = parseInt($("input#deposit").val());
+      let withdraw = parseInt($("input#withdraw").val());
+      let change = new Change(deposit, withdraw);
+      
+      newAccount.depositAmount(change);
+      newAccount.withdrawAmount(change);
+      $("input#deposit").val("");
+      $("input#withdraw").val("");
+      
+      $(".balance").text(newAccount.total); 
+    });
   });
 });
